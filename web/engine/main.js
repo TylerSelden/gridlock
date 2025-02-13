780d91c745d4909369dd7c3f528a1d7896d3fa43
TylerSelden <tyler.selden@salesianum.org>
2025-02-12 21:01:45 -0500

    quick things

--- web/engine/main.js
+++ web/engine/main.js
@@ -1,10 +1,17 @@
 import * as Game from "./game.js";
-
-let size = 16;
+let socket;
 
 export async function init() {
+  socket = new WebSocket("wss://server.benti.dev:8080");
+  window.socket = socket;
+  socket.onmessage = (msg) => {
+    console.log(msg.data);
+  }
+}
+
+async function initEngine() {
   window.Game = await Game.Game.create();
-  window.Game.init(size);
+  window.Game.init(16);
 
   setInterval(() => { window.Game.lightCycle() }, 25);
 }
